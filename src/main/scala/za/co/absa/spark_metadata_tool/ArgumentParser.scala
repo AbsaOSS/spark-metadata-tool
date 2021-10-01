16131fd4b0df3c4b3627dcbf7e6cd058714ebee1
Tomas Dlabka <tomas.dlabka@protonmail.com>
2021-10-01 15:38:07 +0200

    Derive filesystem from path argument (#16)

--- src/main/scala/za/co/absa/spark_metadata_tool/ArgumentParser.scala
+++ src/main/scala/za/co/absa/spark_metadata_tool/ArgumentParser.scala
@@ -15,6 +15,7 @@
 
 package za.co.absa.spark_metadata_tool
 
+import cats.implicits._
 import org.apache.hadoop.fs.Path
 import scopt.OParser
 import za.co.absa.spark_metadata_tool.model.AppConfig
@@ -24,15 +25,9 @@ import za.co.absa.spark_metadata_tool.model.S3
 import za.co.absa.spark_metadata_tool.model.TargetFilesystem
 import za.co.absa.spark_metadata_tool.model.Unix
 import za.co.absa.spark_metadata_tool.model.UnknownError
+import za.co.absa.spark_metadata_tool.model.UnknownFileSystemError
 
 object ArgumentParser {
-
-  implicit val filesystemRead: scopt.Read[TargetFilesystem] = scopt.Read.reads {
-    case "unix" => Unix
-    case "hdfs" => Hdfs
-    case "s3"   => S3
-    case fs     => throw new NoSuchElementException(s"Unrecognized filesystem $fs")
-  }
 
   implicit val hadoopPathRead: scopt.Read[Path] = scopt.Read.reads {
     case s if s.nonEmpty => new Path(s)
@@ -49,11 +44,7 @@ object ArgumentParser {
       opt[Path]('p', "path")
         .required()
         .action((x, c) => c.copy(path = x))
-        .text("path text"),
-      opt[TargetFilesystem]('f', "filesystem")
-        .required()
-        .action((x, c) => c.copy(filesystem = x))
-        .text("unix/hdfs/s3")
+        .text("path text")
     )
   }
 
@@ -67,7 +58,20 @@ object ArgumentParser {
       )
     )
 
-    parseResult.toRight(UnknownError("Unknown error when parsing arguments"))
+    parseResult.fold(Left(UnknownError("Unknown error when parsing arguments")): Either[AppError, AppConfig]) { conf =>
+      val fs = getFsFromPath(conf.path.toString)
+      fs.map(fs => conf.copy(filesystem = fs))
+    }
+  }
+
+  private def getFsFromPath(path: String): Either[UnknownFileSystemError, TargetFilesystem] = path match {
+    case _ if path.startsWith("/")       => Unix.asRight
+    case _ if path.startsWith("hdfs://") => Hdfs.asRight
+    case _ if path.startsWith("s3://")   => S3.asRight
+    case _ =>
+      UnknownFileSystemError(
+        s"Couldn't extract filesystem from path $path"
+      ).asLeft
   }
 
 }
