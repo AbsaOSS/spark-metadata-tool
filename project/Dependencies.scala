928ab5aeb473dc6f41ba58527bfa7329f2e9dd9a
Tomas Dlabka <tomas.dlabka@protonmail.com>
2021-10-08 14:20:58 +0200

    Add assembly logic (#9)
    * Add assembly and publishing logic
    
    * Add missing licenses to sbt files
    
    * Setup publishing to Github
    
    * Remove deprecated syntax
    
    * Hide fat jar dependencies
    
    * Fake PAT for local development

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import sbt._
 
 object Dependencies {
@@ -7,9 +22,11 @@ object Dependencies {
   lazy val spray     = "io.spray"              %% "spray-json" % "1.3.6"
   lazy val scopt     = "com.github.scopt"      %% "scopt"      % "4.0.1"
   lazy val cats      = "org.typelevel"         %% "cats-core"  % "2.3.0"
-  lazy val aws       = "software.amazon.awssdk" % "s3"         % "2.17.38"
+  lazy val aws       = "software.amazon.awssdk" % "s3"         % "2.17.55"
 
-  lazy val hadoop = "org.apache.hadoop" % "hadoop-client" % "2.10.1"
+  lazy val hadoop = ("org.apache.hadoop" % "hadoop-common" % "2.10.1")
+    .exclude("asm", "asm")
+    .exclude("org.mortbay.jetty", "servlet-api")
 
   lazy val dependencies: Seq[ModuleID] = Seq(
     aws,
